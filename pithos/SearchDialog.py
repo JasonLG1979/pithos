2298bff82239133ea05ac5e030edfcc47e63d1c7
JasonLG1979 <jasonlevigray3@gmail.com>
2016-11-05 15:16:10 -0500

    Improved station search dialog Make better use of GtkTemplate Moved search entry to headerbar Added placeholder text to search entry model is no longer populated if the search entry is empty

--- pithos/SearchDialog.py
+++ pithos/SearchDialog.py
@@ -15,52 +15,79 @@
 ### END LICENSE
 
 import html
-from gi.repository import GObject, Gtk
+from gi.repository import GObject, Gtk, Gdk
 
 from .gi_composites import GtkTemplate
 
 @GtkTemplate(ui='/io/github/Pithos/ui/SearchDialog.ui')
 class SearchDialog(Gtk.Dialog):
     __gtype_name__ = "SearchDialog"
+    __gsignals__ = {
+        "add-station": (GObject.SignalFlags.RUN_FIRST, None, (str,)),
+    }
 
     entry = GtkTemplate.Child()
-    treeview = GtkTemplate.Child()
+    search_results_model = GtkTemplate.Child()
+    add_btn = GtkTemplate.Child()
 
-    def __init__(self, *args, **kwargs):
-        self.worker_run = kwargs["worker"]
-        del kwargs["worker"]
-
-        super().__init__(*args, use_header_bar=1, **kwargs)
+    def __init__(self, worker_run, **kwargs):
+        super().__init__(**kwargs)
         self.init_template()
-
-        self.model = Gtk.ListStore(GObject.TYPE_PYOBJECT, str)
-        self.treeview.set_model(self.model)
-
-        self.result = None
+        self.worker_run = worker_run
+        self.query = ''
 
     @GtkTemplate.Callback
-    def search_clicked(self, widget):
-        self.search(self.entry.get_text())
+    def on_cancel_btn_clicked(self, *ignore):
+        self.query = ''
+        # Reset everything.
+        text_in_search_entry = self.entry.get_text()
+        if text_in_search_entry:
+            self.entry.set_text('')
+        self.add_btn.set_sensitive(False)
+        self.search_results_model.clear()
+        self.hide()
 
     @GtkTemplate.Callback
-    def get_selected(self):
-        sel = self.treeview.get_selection().get_selected()
-        if sel[1]:
-            return self.treeview.get_model().get_value(sel[1], 0)
+    def on_add_btn_clicked(self, btn, treeview):
+        musicId = self.get_musicId(treeview)
+        if musicId:
+            self.on_cancel_btn_clicked()
+            self.emit("add-station", musicId)
 
-    def search(self, query):
-        if not query: return
+    @GtkTemplate.Callback
+    def on_entry_search_changed(self, entry, model):
+        self.add_btn.set_sensitive(False)
+        model.clear()
+        self.query = entry.get_text()
+        if not self.query: return
         def callback(results):
-            self.model.clear()
+            model.clear()
+            # If there is no current query(the searchbox is empty)
+            # we don't want to populate the model with irrelevant results.
+            if not self.query: return
             for i in results:
                 if i.resultType is 'song':
                     mk = "<b>%s</b> by %s"%(html.escape(i.title), html.escape(i.artist))
                 elif i.resultType is 'artist':
                     mk = "<b>%s</b> (artist)"%(html.escape(i.name))
-                self.model.append((i, mk))
-            self.treeview.show()
-        self.worker_run('search', (query,), callback, "Searching...")
+                model.append((i.musicId, mk))
+        self.worker_run('search', (self.query,), callback, "Searching...")
 
-    def cursor_changed(self, *ignore):
-        self.result = self.get_selected()
-        self.set_response_sensitive(Gtk.ResponseType.OK, not not self.result)
+    @GtkTemplate.Callback
+    def on_treeview_button_press_event(self, treeview, event):
+        pthinfo = treeview.get_path_at_pos(int(event.x), int(event.y))
+        if pthinfo is not None:
+            path, col, cellx, celly = pthinfo
+            treeview.grab_focus()
+            treeview.set_cursor(path, col, 0)
+        musicId = self.get_musicId(treeview)
+        self.add_btn.set_sensitive(not not musicId)
+        if event.button == 1 and event.type == Gdk.EventType.DOUBLE_BUTTON_PRESS and musicId:
+            self.on_cancel_btn_clicked()
+            self.emit("add-station", musicId)
+
+    def get_musicId(self, treeview):
+        selected = treeview.get_selection().get_selected()[1]
+        if selected:
+            return treeview.get_model().get_value(selected, 0)
+
